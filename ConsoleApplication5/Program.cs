dcd6dbf39ba96586fec76e53de22bc5b5bdbc784
florian-bellotti <florian.bellotti@viacesi.fr>
2016-02-07 16:49:00 +0100

    moyenne des informations sur 30sec

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -1,34 +1,62 @@
 ﻿using System;
 using System.Threading;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 public class TestPerfCounter
 {
     static PerformanceCounter counterDisk;
     static PerformanceCounter counterRAM;
     static PerformanceCounter counterCPU;
-
+    static double infoDisk;
+    static double infoRAM;
+    static double infoCPU;
 
     public static void Main()
     {
-        double infoDisk;
-        double infoRAM;
-        double infoCPU;
-    counterDisk = new PerformanceCounter("PhysicalDisk", @"% Disk Time", @"_Total"); //sup de 90%
+         infoDisk = 0;
+         infoRAM = 0;
+         infoCPU = 0;
+        List<double> listInfoDisk = new List<double>();
+        List<double> listInfoRAM = new List<double>();
+        List<double> listInfoCPU = new List<double>();
+        counterDisk = new PerformanceCounter("PhysicalDisk", @"% Disk Time", @"_Total"); //sup de 90%
         counterCPU = new PerformanceCounter("Processor", @"% Processor Time", @"_Total"); //85%
         counterRAM = new PerformanceCounter("Memory", "Available MBytes"); //moins de 50MB
-        while (true)
+        int count = 0;                      //Compteur
+
+        while (count<=30)
         {
-            infoDisk = counterDisk.NextValue();
-            infoCPU = counterCPU.NextValue();
-            infoRAM = counterRAM.NextValue();
-            Console.WriteLine("Disk : " + infoDisk + @" %; CPU : " + infoCPU + @" %; RAM : " + infoRAM + " MB");
-            if ((infoDisk >= 90) || (infoRAM <= 50) || (infoCPU >= 85))
-            {
-                Console.WriteLine("Erreur !!");
-            }
+            //On récupère les info et on les met dans des listes
+            listInfoDisk.Add(counterDisk.NextValue());
+            listInfoCPU.Add(counterCPU.NextValue());
+            listInfoRAM.Add(counterRAM.NextValue());
+
+            //on calcule la moyenne de chaque liste
+            infoCPU = calculMoyenne(listInfoCPU);
+            infoRAM = calculMoyenne(listInfoRAM);
+            infoDisk = calculMoyenne(listInfoDisk);
+
+            Console.WriteLine(count);
+            count++;
             Thread.Sleep(1000);
-            Console.WriteLine(@"-------------------------------------");
+           
         }
+
+        Console.WriteLine("Disk : " + infoDisk + @" %; CPU : " + infoCPU + @" %; RAM : " + infoRAM + " MB");
+        Console.Read();
+    }
+
+    static double calculMoyenne(List<double> liste)
+    {
+        double total = 0;
+
+        for (int i = 0; i < liste.Count; i++)
+        {
+            total += liste[i];
+        }
+
+        return total / liste.Count;
     }
 }
+
