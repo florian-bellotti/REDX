e36fe29c37f44d7b4f8118c8e2665f45814eec3b
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-08 13:13:30 +0100

    Ajout de l'observateur

---
+++ ConsoleApplication5/Observable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    interface Observable
+    {
+        void ajouterObs(Observer o);
+        void supprimerObs(Observer o);
+        void notifierObs();
+        int[] getAllStates();
+
+    }
+}
---
+++ ConsoleApplication5/ObserverCPU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class ObserverCPU : Observer
+    {
+        public void actualiser(Observable o)
+        {
+            int[] tabValeurCpu = o.getAllStates();
+            Console.WriteLine("Cpu : " + tabValeurCpu[0]);
+            Console.WriteLine("Ram : " + tabValeurCpu[1]);
+            Console.WriteLine("Espace disque : " + tabValeurCpu[2]);
+
+        }
+    }
+}
