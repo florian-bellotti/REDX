c2471cde2572af5390c75773d870f6068150d6ca
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-08 17:05:41 +0100

    Suppresion fichier inutile

--- Database1/MachineInformation.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[MachineInformation]
-(
-	[Id_performance] INT NOT NULL PRIMARY KEY, 
-    [date] DATETIME NULL, 
-    [RAM] INT NULL, 
-    [CPU] INT NULL, 
-    [disque] INT NULL
-)
