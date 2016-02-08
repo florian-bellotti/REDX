258c25b4c1e2359f6ccfe4b84741c7d48528d4ba
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-08 16:29:39 +0100

    ...

--- Database1/MachineInformation.sql
+++ Database1/MachineInformation.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[MachineInformation]
 (
-	[Id_information] INT NOT NULL PRIMARY KEY, 
+	[Id_performance] INT NOT NULL PRIMARY KEY, 
     [date] DATETIME NULL, 
     [RAM] INT NULL, 
     [CPU] INT NULL, 
-    [disque] INT NULL, 
-    [Id_machine] INT NOT NULL
+    [disque] INT NULL
 )
