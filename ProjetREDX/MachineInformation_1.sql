b0f0fe35829b58f54dd9e7a93e3751f3c10512fa
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-08 15:45:30 +0100

    Test BDD

---
+++ ProjetREDX/MachineInformation_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MachineInformation]
+(
+	[Id_information] INT NOT NULL PRIMARY KEY, 
+    [date] DATETIME NULL, 
+    [RAM] INT NULL, 
+    [CPU] INT NULL, 
+    [disque] INT NULL, 
+    [Id_machine] INT NOT NULL
+)
