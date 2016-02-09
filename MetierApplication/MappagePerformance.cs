65d9b3ca1d8e636a3a11fbd0f3dd7f16cdcbc23a
florian-bellotti <florian.bellotti@viacesi.fr>
2016-02-09 17:43:18 +0100

    test

--- MetierApplication/MappagePerformance.cs
+++ MetierApplication/MappagePerformance.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DonneeApplication;
+using Donnee;
 using System.Data;
 
 namespace MetierApplication
