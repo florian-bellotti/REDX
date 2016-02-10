ee690e0494be10ac5d3fce0411e96984dceb0dd5
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-10 13:43:45 +0100

    Pas encore fini (Premier test raté)

--- ClientDiffusion/FormDiffusion.cs
+++ ClientDiffusion/FormDiffusion.cs
@@ -7,17 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.IO;
+using MetierDiffusion;
 
 namespace ClientDiffusion
 {
     public partial class FormDiffusion : Form
     {
+        public KinectSensor sensor = null;
+        static MoveRightGesture _gesture = new MoveRightGesture();
         public FormDiffusion()
         {
             InitializeComponent();
+            initializeKinect();
 
             //initialisation des composants du media player
-            WMPLib.IWMPPlaylist playlist = mediaPlayer.newPlaylist("myPlaylist", string.Empty);
+            /*WMPLib.IWMPPlaylist playlist = mediaPlayer.newPlaylist("myPlaylist", string.Empty);
             string[] lines = System.IO.File.ReadAllLines(@"playlist.txt");
 
             foreach (string path in lines)
@@ -31,7 +37,63 @@ namespace ClientDiffusion
 
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.Height = 1032;
-            this.Width = 1632;
+            this.Width = 1632;*/
+
+        }
+
+        private void initializeKinect()
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+            if (null != this.sensor)
+            {
+                this.sensor.SkeletonStream.Enable();
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+                _gesture.GestureRecognized += Gesture_GestureRecognized;
+
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+        }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                Skeleton[] skeletons = new Skeleton[frame.SkeletonArrayLength];
+                if (frame != null)
+                {
+                    frame.CopySkeletonDataTo(skeletons);
+
+                    if (skeletons.Length > 0)
+                    {
+                        var user = skeletons.Where(
+                                   u => u.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+
+                        if (user != null)
+                        {
+                            _gesture.Update(user);
+                        }
+                    }
+                }
+            }
+        }
+        public void Gesture_GestureRecognized(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.pause();
+            Console.WriteLine("Gesture reconnue");
         }
 
         private void mediaPlayer_Enter(object sender, EventArgs e)
---
+++ MetierDiffusion/MoveRightSegment1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace MetierDiffusion
+{
+    public class MoveRightSegment1 : IGestureSegment
+    {
+        public GesturePartResult Update(Skeleton skeleton)
+        {
+            // Main au dessus du coude
+            if (skeleton.Joints[JointType.HandRight].Position.Y >
+                skeleton.Joints[JointType.ElbowRight].Position.Y)
+            {
+                // Main a la même position que le coude
+                if (skeleton.Joints[JointType.HandRight].Position.X ==
+                    skeleton.Joints[JointType.ElbowRight].Position.X)
+                {
+                    return GesturePartResult.Succeeded;
+                }
+            }
+
+            // Hand dropped
+            return GesturePartResult.Failed;
+        }
+    }
+}
