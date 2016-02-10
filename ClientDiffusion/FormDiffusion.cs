dde3aee193be8a5a148ce3e2c022d6fd49ea79f4
Thibaut CHIEUX <thibaut.chieux@viacesi.fr>
2016-02-10 15:26:21 +0100

    Affichage du squelette (Seulement)

--- ClientDiffusion/FormDiffusion.cs
+++ ClientDiffusion/FormDiffusion.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Kinect;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
 
 namespace ClientDiffusion
 {
     public partial class FormDiffusion : Form
     {
-        public KinectSensor sensor = null;
-        //static MoveRightGesture _gesture = new MoveRightGesture();
+       
         public FormDiffusion()
         {
             InitializeComponent();
+            /*initializeKinect();
 
             //initialisation des composants du media player
             WMPLib.IWMPPlaylist playlist = mediaPlayer.newPlaylist("myPlaylist", string.Empty);
@@ -34,12 +37,9 @@ namespace ClientDiffusion
 
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.Height = 1032;
-            this.Width = 1632;
+            this.Width = 1632;*/
         }
 
-        private void mediaPlayer_Enter(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
